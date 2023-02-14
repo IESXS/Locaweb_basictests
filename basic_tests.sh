17812b156cf728758f557040b0125c418e8ecf28
IESXS <52583982+IESXS@users.noreply.github.com>
2023-02-14 14:23:39 -0300

    Update basic_tests.sh

--- basic_tests.sh
+++ basic_tests.sh
@@ -17,13 +17,7 @@ echo "para qual usuario de ftp esta realizando o ajuste?"
 read ftpuser
 
 mv php.ini php.ini_cliente123 && cp/etc/php.ini . &&
-sed-i 's/max_execution_time= 30/max_execution_time= 90/' php.ini &&
-sed-i 's/max_input_time= 60/max_input_time= 90/' php.ini &&
-sed-i 's/memory_limit= 32M/memory_limit= 512M/' php.ini &&
-sed-i 's/post_max_size= 8M/post_max_size= 50M/' php.ini &&
-sed-i 's/upload_max_filesize= 2M/upload_max_filesize= 50M/' php.ini &&
-sed-i '954i session.save_path= "/home/'$USER'/tmp"' php.ini &&
-sed-i '955d' php.ini
+wget http://raw.githubusercontent.com/IESXS/Locaweb_basictests/main/php.ini
 
 echo "----------------- Ajustes no htaccess -------------------"
 echo "Aperte enter para realizar a alteração do htaccess"
